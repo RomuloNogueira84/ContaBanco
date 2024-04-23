bca761a29335c9eff951d0afa7834398125cf71f
Romulo Nogueira <romulonogueira929@gmail.com>
2024-04-23 18:26:39 -0300

    noviddes

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ContaTerminal {
-}
+import java.util.Scanner;
+
+    public class ContaTerminal {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Por favor, digite o número da conta:");
+            int numero = scanner.nextInt();
+
+            System.out.println("Por favor, digite o número da agência:");
+            scanner.nextLine(); // Consume newline left-over
+            String agencia = scanner.nextLine();
+
+            System.out.println("Por favor, digite o nome do cliente:");
+            String nomeCliente = scanner.nextLine();
+
+            System.out.println("Por favor, digite o saldo inicial:");
+            double saldo = scanner.nextDouble();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        }
+    }
